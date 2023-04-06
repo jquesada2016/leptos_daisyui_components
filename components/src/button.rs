f1e3bea8a458c42e2b1a734ea7af3a11492b53ef
Jose Quesada <jquesada2016@fau.edu>
2023-04-06 11:43:07 -0500

    fix typo

--- components/src/button.rs
+++ components/src/button.rs
@@ -177,7 +177,7 @@ pub fn Button(
     .node_ref(btn_ref_local)
     .class("btn", true)
     .class("btn-outline", outlined)
-    .class("bgn-ghost", ghost)
+    .class("btn-ghost", ghost)
     .class("loading", loading)
     .class("btn-active", active)
     .class("no-animation", no_animation)
